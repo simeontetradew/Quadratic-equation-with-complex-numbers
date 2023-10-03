0c2c5c6748a6ea7b6d9ef09372e184e0f8aabe8b
Simeon Tetradev <146888424+simeontetradew@users.noreply.github.com>
2023-10-04 01:52:37 +0300

    Version 1

--- quadequ.py
+++ quadequ.py
@@ -1,12 +1,12 @@
 import math
-a = float(input('a: '))
+a = float(input('a: ')) 
 b = float(input('b: '))
 c = float(input('c: '))
 d = b**2 - 4*a*c
-if (a == 0):
-    print('a = 0')
+if (a == 0): # 'a' cannot be zero as a division by zero is not possible.
+    print('a = 0') 
 else:
-    if (d < 0):
+    if (d < 0): # Turns 'd' into an imaginary number when its value is below zero and prints out the solutions as complex numbers.
         d = math.sqrt(-d)
         i = 1j
         d = d*i
